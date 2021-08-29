afd33c40bb09fbd807c71bba7ad1771184ca1143
Tony Spencer <aspencer.ignotus@gmail.com>
2021-08-28 18:03:09 -0700

    keyval db and recipe db stores

---
+++ src/data/db.js
@@ -0,0 +1,60 @@
+import { openDB } from 'idb/with-async-ittr.js';
+
+import { upgradeSchema } from '../lib/db';
+import { upgradeRecipeSchema } from './recipe';
+
+export const DB_NAME = 'VKUS';
+const DB_VERSION = 1;
+
+export const KEYVAL_STORE = 'app';
+
+const dbPromise = () => openDB(DB_NAME, DB_VERSION, {
+	upgrade(db, oldVersion, newVersion, tx) {
+
+		// General changes related to the app, keyval store
+		const changes = {
+			1: ({ db }) => {
+				// Key-Value Store for the app
+				db.createObjectStore(KEYVAL_STORE);
+			},
+		};
+		upgradeSchema({
+			changes,
+			db,
+			oldVersion,
+			newVersion,
+			tx,
+		});
+
+		// Recipe Schema changes
+		upgradeRecipeSchema({
+			changes,
+			db,
+			oldVersion,
+			newVersion,
+			tx,
+		});
+
+	},
+});
+
+export async function vkusDB() {
+	const db = await dbPromise();
+	return db;
+}
+
+export async function getValue(key) {
+	return (await dbPromise()).get(KEYVAL_STORE, key);
+};
+export async function setValue(key, val) {
+	return (await dbPromise()).put(KEYVAL_STORE, val, key);
+};
+export async function deleteValue(key) {
+	return (await dbPromise()).delete(KEYVAL_STORE, key);
+};
+export async function clearValues() {
+	return (await dbPromise()).clear(KEYVAL_STORE);
+};
+export async function getValues() {
+	return (await dbPromise()).getAllKeys(KEYVAL_STORE);
+};
--- src/data/recipe.js
+++ src/data/recipe.js
@@ -1,5 +1,9 @@
 import { create } from './model';
 
+import { upgradeSchema } from '../lib/db';
+
+export const RECIPE_STORE = 'recipes';
+
 /**
  * @typedef {Object} Recipe
  * @property {!number} id Unique Identifier for the recipe
@@ -7,7 +11,7 @@ import { create } from './model';
  * @property {?string} image Link to image for recipe card
  * @property {?string} link External link to recipe (paprika, blog, online)
  * @property {[string]} tags Applicable tags (ex: 'kid-friendly', 'quick', 'vegan')
- * @property {number} lastUsed Timestamp for when the recepe was last in a plan
+ * @property {number} lastUsed Timestamp for when the recipe was last in a plan
  */
 
 /** @type Recipe */
@@ -17,7 +21,7 @@ export const recipe = {
 	image: null,
 	link: null,
 	tags: [],
-	lastUsed: null,
+	lastUsed: 0,
 };
 
 /**
@@ -28,4 +32,68 @@ export const recipe = {
  */
 export const createRecipe = function (targetRecipe={}) {
 	return create(recipe, targetRecipe);
+};
+
+/**
+ * Add a new recipe onto the store
+ *
+ * @param {object} db Wrapped IndexedDB Object
+ * @param {Recipe} targetRecipe
+ * @returns {Recipe}
+ */
+export const addRecipe = async (db, targetRecipe) => {
+	const recipe = createRecipe(targetRecipe);
+
+	// default key will be returned
+	delete recipe.id;
+
+	recipe.id = await db.add(RECIPE_STORE, recipe);
+
+	return recipe;
+};
+
+/**
+ * Add a new recipe onto the store
+ *
+ * @param {object} db Wrapped IndexedDB Object
+ * @param {any} key
+ * @returns {Recipe}
+ */
+export const getRecipe = async (db, key) => {
+	return await db.get(RECIPE_STORE, key);
+};
+
+/**
+ * Recipe Schema
+ *
+ * @param {object} params
+ * @param {object} params.db IDB Object
+ * @param {number} params.oldVersion Old/Current DB Version
+ * @param {number} params.newVersion New DB version
+ * @param {object} params.tx IDB upgrade transaction
+ */
+export const upgradeRecipeSchema = function ({ db, oldVersion, newVersion, tx }) {
+
+	const changes = {
+		1: ({db}) => {
+			// Create a store of objects
+			const store = db.createObjectStore(RECIPE_STORE, {
+				// The 'id' property of the object will be the key.
+				keyPath: 'id',
+				// If it isn't explicitly set, create a value by auto incrementing.
+				autoIncrement: true,
+			});
+			// Create an index on the 'date' property of the objects.
+			store.createIndex('lastUsed', 'lastUsed');
+			store.createIndex('title', 'title');
+		},
+	};
+
+	upgradeSchema({
+		changes,
+		db,
+		oldVersion,
+		newVersion,
+		tx,
+	});
 };
